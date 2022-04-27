aec547139d0dfd097b359529f1b8f16f0bee1c8f
TesvintsevDev <tesvintsev@gmail.com>
2022-04-27 14:57:07 +0300

    improvement of the table

--- src/components/request/RequestTable.vue
+++ src/components/request/RequestTable.vue
@@ -16,8 +16,8 @@
               <td>{{ idx + 1 }}</td>
               <td>{{ r.fio }}</td>
               <td>{{ r.phone }}</td>
-              <td>{{ r.amount }}</td>
-              <td>{{ r.status }}</td>
+              <td>{{ currency(r.amount) }}</td>
+              <td><AppStatus :type="r.status" /></td>
               <td>
                 <router-link v-slot="{navigate}" custom :to="{name: 'Request', params: {id: r.id}}">
                   <button class="btn primary" @click="navigate">Открыть</button>
@@ -29,8 +29,15 @@
 </template>
 
 <script>
+import {currency} from '../../utils/currency'
+import AppStatus from '../ui/AppStatus'
+
 export default {
-    props: ['requests']
+    props: ['requests'],
+    setup() {
+      return {currency}
+    },
+    components: {AppStatus}
 }
 </script>
 
---
+++ src/components/ui/AppStatus.vue
@@ -0,0 +1,47 @@
+<template>
+  <span :class="['badge', className]">{{text}}</span>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+    props: {
+        type: {
+            type: String,
+            required: true,
+            validator(value) {
+                return ['active', 'cancelled', 'done', 'pending'].includes(value)
+            }
+        }
+    },
+    setup(props) {
+        const classesMap = {
+            active: 'primary',
+            cancelled: 'danger',
+            done: 'primary',
+            pending: 'warning'
+        }
+
+        const textMap = {
+            active: 'Активен',
+            cancelled: 'Отменен',
+            done: 'Завершен',
+            pending: 'Выполняется'
+        }
+
+        const className = ref(classesMap[props.type])
+        const text = ref(textMap[props.type])
+
+        return {
+            className,
+            text
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
