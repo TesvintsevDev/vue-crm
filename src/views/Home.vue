4991233d6458c81de8fbeeb1c45d10f93ff25b91
TesvintsevDev <tesvintsev@gmail.com>
2022-04-29 18:31:26 +0300

    add dynamic filter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
       <button class="btn primary" @click="modal = true">Создать</button>
     </template>
     
+    <request-filter v-model="filter"></request-filter>
     <request-table :requests="requests"></request-table>
 
 
@@ -21,15 +22,19 @@ import {ref, computed, onMounted} from 'vue'
 import { useStore } from 'vuex'
 import AppPage from '../components/ui/AppPage'
 import RequestTable from '../components/request/RequestTable'
+import RequestFilter from '../components/request/RequestFilter'
 import AppModal from '../components/ui/AppModal'
 import RequestModal from '../components/request/RequestModal'
 import AppLoader from '../components/ui/AppLoader'
+
 
 export default {
   setup() {
     const store = useStore()
     const modal = ref(false)
     const loading = ref(false)
+    const filter = ref({})
+
 
     onMounted( async () => {
       loading.value = true
@@ -37,14 +42,30 @@ export default {
       loading.value = false
     })
 
-    const requests = computed( () => store.getters['request/requests'])
+    const requests = computed( () => store.getters['request/requests']
+        .filter(request => {
+          if (filter.value.name) {
+            return request.fio.includes(filter.value.name)
+          }
+          return request
+        })
+        .filter(request => {
+          if (filter.value.status) {
+            return filter.value.status === request.status
+          }
+          return request
+        })
+        
+    )
 
     return {
-      modal, requests, loading
+      modal, requests, loading, filter
     }
   },
   components: {
-    AppPage, RequestTable, AppModal, RequestModal, AppLoader
+    AppPage, RequestTable, AppModal, RequestModal, AppLoader, RequestFilter
     }
 }
 </script>
+
+ 
