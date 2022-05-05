f5b719ca1de03eb87c3c8fd31aadaa48470adfd8
TesvintsevDev <tesvintsev@gmail.com>
2022-05-05 15:23:34 +0300

    add new component <Help>

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="help">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Помощь">
+    <question-item
+      v-for="q in questions"
+      :key="q.title"
+      :question="q"
+    ></question-item>
+  </app-page>
+  
 </template>
+
+<script>
+import AppPage from '../components/ui/AppPage.vue'
+import QuestionItem from '../components/question/Questionitem.vue'
+
+export default {
+  setup() {
+    const questions = [
+      {title: 'Что есть в данной системе?', text: 'Тут есть все, для того, чтобы смотреть, какие заявки человек добавлял'},
+      {title: 'Как тут работает авторизация?', text: 'Все реализовано через Firebase'}
+    ]
+
+    return {questions}
+  },
+  components: {AppPage, QuestionItem}
+}
+</script>
+
+<style>
+
+</style>
